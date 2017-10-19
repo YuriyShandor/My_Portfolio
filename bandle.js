69712a6cad5ff82f7d7ba220e48b1bc093f798e9
Yuriy Shandor <yuriyshandor@gmail.com>
2017-10-19 11:47:01 +0300

    Add Mod_09

--- bandle.js
+++ bandle.js
@@ -88,6 +88,19 @@ $(document).ready(function () {
   });
 });
 
+function getAge(dateString) {
+  var today = new Date();
+  var birthDate = new Date(dateString);
+  var age = today.getFullYear() - birthDate.getFullYear();
+  var m = today.getMonth() - birthDate.getMonth();
+  if (m < 0 || m === 0 && today.getDate() < birthDate.getDate()) {
+    age--;
+  }
+  return age;
+}
+
+document.getElementById('my-age').innerHTML = getAge("1993/05/23");
+
 /***/ }),
 /* 2 */
 /***/ (function(module, exports) {

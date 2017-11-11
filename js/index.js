93354356f017edb4b2f3aaa9c5fbeba80c2f1b0e
Yuriy Shandor <yuriyshandor@gmail.com>
2017-11-11 18:36:47 +0200

    Add Mod_10

--- js/index.js
+++
@@ -1,8 +0,0 @@
-// import './js/jquery-3.2.1.min.js';
-//import './js/bootstrap.js';
-// import './js/jquery.nice-select.js';
-// import './js/slick.js';
-
-import './bin/script';
-
-import '../scss/base.scss';
